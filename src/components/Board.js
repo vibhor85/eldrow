07fa4af6cd3f9e895c6ed778e7c5345668cb8b24
Vibhor Kumar <55327046+vibhor85@users.noreply.github.com>
2023-09-01 10:41:57 +0000

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,6 +5,7 @@ import Word from "./Word";
 const Board = () => {
   const [active, setActive] = useState([true, false, false, false, false]);
   const { word } = useContext(wordleStore);
+  console.log(word);
   return (
     <div className="board">
       {active.map((ac, i) => (
